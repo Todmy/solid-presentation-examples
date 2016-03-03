469cfa03cbe1521d0227cfc446ddaa964ef20ff3
Todmy <tolok.d@gmail.com>
2016-03-03 23:41:48 +0200

    Added bad example for SRP (es6)

--- es6/single-responsibility-principle/srp1-bad.js
+++ es6/single-responsibility-principle/srp1-bad.js
@@ -1,23 +1,21 @@
-function User(options) {
-  this._name = options.name;
-  this._email = options.email;
+class Book {
+  constructor ({ author, title }) {
+    console.log(author, title)
+  	Object.assign(this, { author, title })
+  }
+
+  getAuthor() {
+    console.log('Author is ' + this.author);
+  }
+
+  getTitle() {
+    console.log('Title is ' + this.title);
+  }
+
+  print() {
+  	console.log('Page is printed');
+  }
 }
 
-User.prototype.newLetter = function(attributes){
-  this._recipient = attributes.recipient;
-  this._title = attributes.title;
-  this._text = attributes.text;
-};
-
-User.prototype.sendLetter = function() {
-  console.log('send letter ---> ', {
-    sender: this._email,
-    recipient: this._recipient,
-    title: this._title,
-    text: this._text + ' from ' + this._name
-  });
-};
-
-var user = new User({ name: 'Bob', email: 'bob@test.com' });
-user.newLetter({ recipient: 'Bill@doom.com', title: 'title', text: 'text' });
-user.sendLetter();
+var myBook = new Book({ author: 'Joanne "Jo" Rowling', title: 'Harry Potter' });
+myBook.print()
